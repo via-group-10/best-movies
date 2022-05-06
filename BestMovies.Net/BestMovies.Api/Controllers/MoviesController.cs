673ac09908726469b42a97be2b839314894b203f
Anton Janto <anton.janto@gmail.com>
2022-05-06 18:21:12 +0200

    projects commit

---
+++ BestMovies.Net/BestMovies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using BestMovies.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestMovies.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MoviesController : ControllerBase
+{
+
+    [HttpGet(Name = "GetMovies")]
+    public async Task<ActionResult<IEnumerable<Movie>>> Get()
+    {
+        var res = await Task.FromResult(Enumerable.Range(1, 5).Select(x => new Movie
+        {
+            Description = $"{x}"
+        }));
+        return Ok(res);
+    }
+}
---
+++ BestMovies.Net/BestMovies.Api/Models/Movie.cs
@@ -0,0 +1,7 @@
+﻿namespace BestMovies.Api.Models;
+
+public class Movie
+{
+    public string Name { get; set; } = "Meno filmu";
+    public string? Description { get; set; }
+}
