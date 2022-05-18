7c96cdb9d6dc802522cee8e53b3f525c2db0a4a6
BigBrainer <galamarek147@gmail.com>
2022-05-18 19:45:34 +0200

    movie carousel functional

---
+++ best-movies-web/src/Search/SearchResult.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        const res = await fetch('http://localhost:5252/api/movies?title={}')
+        movies = await res.json();
+    })
+</script>
