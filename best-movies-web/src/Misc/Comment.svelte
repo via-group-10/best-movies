79faedcf7553871865cae01f84ab8aa0cabca91e
Anton Janto <anton.janto@gmail.com>
2022-05-25 14:03:47 +0200

    added comments for ui

---
+++ best-movies-web/src/Misc/Comment.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let comment;
+</script>
+
+<div class="card-body">
+  <div class="d-flex flex-start">
+    <img
+      class="rounded-circle shadow-1-strong me-3"
+      src="images/avatar.svg"
+      alt="avatar"
+      width="60"
+      height="60"
+    />
+    <div>
+      <h6 class="fw-bold mb-1">{ comment.username }</h6>
+      <div class="d-flex align-items-center mb-3">
+        <p class="mb-0">
+          { comment.created }
+        </p>
+      </div>
+      <p class="mb-0">
+          { comment.text }
+      </p>
+    </div>
+  </div>
+</div>
--- best-movies-web/src/routes.js
+++ best-movies-web/src/routes.js
@@ -2,13 +2,11 @@ import Home from './Home/Home.svelte';
 import MovieDetail from './Movies/MovieDetail.svelte';
 import NotFound from './Misc/NotFound.svelte';
 import SearchResult from './Search/SearchResult.svelte'
-import MovieComments from './Movies/MovieComments.svelte'
 
 export default {
     "/": Home,
     "/movie/:id": MovieDetail,
     "/search/:filter": SearchResult,
-    "/movie/comments/:movie_id": MovieComments,
     // If route not recognized:
     "*": NotFound
 }
