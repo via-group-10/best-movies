37eeaf27b31cb218a16a04639ebd96740b75ccec
Anton Janto <anton.janto@gmail.com>
2022-05-26 14:41:52 +0200

    why no work

--- best-movies-web/src/routes.js
+++ best-movies-web/src/routes.js
@@ -2,7 +2,6 @@ import Home from './Home/Home.svelte';
 import MovieDetail from './Movies/MovieDetail.svelte';
 import NotFound from './Misc/NotFound.svelte';
 import SearchResult from './Search/SearchResult.svelte';
-import MovieComments from './Movies/MovieComments.svelte';
 import SignUp from './Authentication/SignUp.svelte';
 import SignIn from './Authentication/SignIn.svelte';
 
