4f141e227a384feeaf48615f887ada115da0b524
BigBrainer <galamarek147@gmail.com>
2022-05-30 16:00:44 +0200

    UI changes

--- best-movies-web/src/Search/SearchResult.svelte
+++ best-movies-web/src/Search/SearchResult.svelte
@@ -3,18 +3,41 @@
     import MovieListItem from "../Movies/MovieListItem.svelte";
     import Spinner from "../Misc/Spinner.svelte";
     import { getMovies } from "../api";
+    import { inview } from "svelte-inview/dist/index";
+    import { Alert, Icon } from 'sveltestrap';
 
     export let params = {};
     let filter = params.filter;
     let movies;
+    let hasMoreResults = true;
 
     onMount(async () => {
-        const res = await getMovies({
-            title: filter
-        });
-        if (res.ok)
-            movies = await res.json();
+        fetchData(0)
     })
+
+    const fetchData = async (offset) => {
+        const res = await getMovies({
+            title: filter,
+            offset: offset,
+            limit: 5
+        });
+        let resBody = await res.json();
+        if (res.ok && resBody.length > 0)
+        {
+            movies = !movies ? resBody: [...movies, ...resBody];
+        }
+        else{
+            hasMoreResults = false;
+        }
+    }
+
+    const handleChange = (e) => {
+
+        if(e.detail.inView && hasMoreResults)
+        {
+            fetchData(movies.length);
+        }
+    }
 
 </script>
 
@@ -22,12 +45,23 @@
 
     {#if movies}
 
-        {#each movies as movie, i (movie.id)}
+        {#each movies as movie}
             <MovieListItem movie={movie}></MovieListItem>
         {/each}
 
+        <div use:inview={{}} on:change={handleChange} />
+
     {:else}
         <Spinner />
+    {/if}
+
+    
+    
+
+    {#if !hasMoreResults}
+        <Alert color = "info" class="text-center my-5">
+            <h4 class="alert-heading text-capitalize"> <Icon name="info-circle-fill"/> Has no more results</h4>
+        </Alert>
     {/if}
 
 </div>
