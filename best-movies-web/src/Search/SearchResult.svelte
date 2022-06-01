3cb9c270b351c097ea91de43beb04b042c4076ae
BigBrainer <galamarek147@gmail.com>
2022-06-01 15:28:50 +0200

    front-end authentication fix

--- best-movies-web/src/Search/SearchResult.svelte
+++ best-movies-web/src/Search/SearchResult.svelte
@@ -4,7 +4,9 @@
     import Spinner from "../Misc/Spinner.svelte";
     import { getMovies } from "../api";
     import { inview } from "svelte-inview/dist/index";
-    import { Alert, Icon } from 'sveltestrap';
+    import { Alert, Icon } from "sveltestrap";
+    import { push } from "svelte-spa-router";
+    import { User } from '../store';
 
     export let params = {};
     let filter = params.filter;
@@ -12,64 +14,61 @@
     let hasMoreResults = true;
 
     onMount(async () => {
-        fetchData(0)
-    })
+        fetchData(0);
+    });
 
-    const fetchData = async (offset) => {
-        const res = await getMovies({
+    const fetchData = (offset) => {
+        getMovies({
             title: filter,
             offset: offset,
-            limit: 5
-        });
-        let resBody = await res.json();
-        if (res.ok && resBody.length > 0)
-        {
-            movies = !movies ? resBody: [...movies, ...resBody];
-        }
-        else{
-            movies = [];
-            hasMoreResults = false;
-        }
-    }
+            limit: 5,
+        })
+            .then((res) => {
+                if (res.ok) {
+                    res.json().then((resBody) => {
+                        if (resBody.length > 0) {
+                            movies = !movies
+                                ? resBody
+                                : [...movies, ...resBody];
+                        } else {
+                            movies = [];
+                            hasMoreResults = false;
+                        }
+                    });
+                }
+            })
+            .catch((e) => {
+                User.signout();
+                push("/signin");
+            });
+    };
 
     const handleChange = (e) => {
-
-        if(e.detail.inView && hasMoreResults)
-        {
+        if (e.detail.inView && hasMoreResults) {
             fetchData(movies.length);
         }
-    }
-
+    };
 </script>
 
 <div class="container pb-4">
-
     {#if movies}
-
         {#each movies as movie}
-            <MovieListItem movie={movie}></MovieListItem>
+            <MovieListItem {movie} />
         {/each}
 
         <div use:inview={{}} on:change={handleChange} />
-
     {:else}
         <Spinner />
     {/if}
 
-    
-    
-
     {#if !hasMoreResults}
-        <Alert color = "info" class="text-center my-5">
-            <h4 class="alert-heading text-capitalize"> <Icon name="info-circle-fill"/> No results</h4>
+        <Alert color="info" class="text-center my-5">
+            <h4 class="alert-heading text-capitalize">
+                <Icon name="info-circle-fill" /> No results
+            </h4>
         </Alert>
     {/if}
-
 </div>
 
-
-
-
 <style>
-
 </style>
